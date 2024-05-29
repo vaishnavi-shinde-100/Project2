b6f4ddcf5733b1d8fd71d3c90ec48a4102d8a1e9
Vaishnavi Shinde <shindevaishnavi992@gmail.com>
2024-05-29 22:33:33 +0530

    Add welcome line

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
+
 //add new feature - button
 //add new feature - form
 
-
-=======
-
-//add new feature - button
-
-//add new feature - form
-
->>>>>>> d1143c934935456b7af6fb4eeeb60b5ea2cfb29f
+console.log("Welcome to my github account");
